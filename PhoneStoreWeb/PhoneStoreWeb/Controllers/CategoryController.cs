c6c3d0eced1104ef130bd2718bd4dee48ec41687
nkhang72 <khangtran722001@gmail.com>
2023-03-07 12:17:25 +0700

    K upload phan loai trang san pham

--- PhoneStoreWeb/PhoneStoreWeb/Controllers/CategoryController.cs
+++ PhoneStoreWeb/PhoneStoreWeb/Controllers/CategoryController.cs
@@ -3,15 +3,36 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Data.Entity;
+using PhoneStoreWeb.Models;
 
 namespace PhoneStoreWeb.Controllers
 {
     public class CategoryController : Controller
     {
-        // GET: Category
+        PhoneStoreEntities1 db = new PhoneStoreEntities1();
         public ActionResult Index()
         {
+
+            var v = from t in db.tb_Product
+                    where t.Hide == true
+                    select t;
             return View();
+        }
+
+        public ActionResult getProductbyMeta(String meta)
+        {
+            var v = from t in db.tb_Product
+                    where t.Meta== meta
+                    select t;
+            return View(v.ToList());
+        }
+        public ActionResult getCategoriesSlideBar()
+        {
+            var v = from t in db.tb_ProductCategory
+                    where t.Hide == true
+                    select t;
+            return PartialView(v.ToList());
         }
     }
 }
--- PhoneStoreWeb/PhoneStoreWeb/Controllers/ContactController.cs
+++ PhoneStoreWeb/PhoneStoreWeb/Controllers/ContactController.cs
@@ -3,15 +3,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Data.Entity;
+using PhoneStoreWeb.Models;
 
 namespace PhoneStoreWeb.Controllers
 {
     public class ContactController : Controller
     {
-        // GET: Contact
+        PhoneStoreEntities1 db = new PhoneStoreEntities1();
         public ActionResult Index()
         {
             return View();
         }
     }
+        
 }
